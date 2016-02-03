70f9108288626051b67fb3c40df3e3120ee7fc1e
vinodramr <vinodram.ramavath@gmail.com>
2016-02-04 02:19:11 +0530

    First Change added output change

--- JunitTest/src/testing/JunitTesting.java
+++ JunitTest/src/testing/JunitTesting.java
@@ -3,10 +3,13 @@ package testing;
 public class JunitTesting {
 
 	public int square(int x){
+		System.out.println("In Square Method");
 		return x*x;
 	}
 	
 	public int countLetters(String word){
 		 return word.length();
 	}
+	
+
 }
